92257c3849fdb8f4a7d02f51411da933a3eed8cd
jlmack13 <jennimacklin@gmail.com>
2018-08-28 14:52:11 -0400

    added create and show method to card controller

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -2,4 +2,21 @@ class Api::CardsController < ApplicationController
     def index
         render json: Card.all
     end
+
+    def show
+        render json: Card.find_by(id: params[:id])
+    end
+    
+    def create
+        card = Card.new(card_params)
+        if card.save
+            render json:
+        end
+    end
+
+    private
+
+    def card_params
+        params.require(:card).permit(:term, :definition, :deck_id)
+    end
 end

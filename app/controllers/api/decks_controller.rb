7543bb8ed010d1d6b406d0513dbb00424d546ea9
jlmack13 <jennimacklin@gmail.com>
2018-08-28 14:46:05 -0400

    added show and create controller actions to deck

--- app/controllers/api/decks_controller.rb
+++ app/controllers/api/decks_controller.rb
@@ -2,4 +2,12 @@ class Api::DecksController < ApplicationController
     def index
         render json: Deck.all
     end
-end
+
+    def show
+        render json: Deck.find_by(id: params[:id])
+    end
+
+    def create
+        deck = Deck.new(deck_params)
+    end
+end 

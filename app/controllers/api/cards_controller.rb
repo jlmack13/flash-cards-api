4bca5811f86f2a70fab026cd4ef444e4d87c4088
jlmack13 <jennimacklin@gmail.com>
2018-09-04 12:29:47 -0400

    adding edit and delete controller actions

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,11 +1,13 @@
 class Api::CardsController < ApplicationController
     #need a before action here for show, edit, and delete
+    before_action :set_card, only: [:show, :edit, :delete]
+
     def index
         render json: Card.all
     end
 
     def show
-        render json: Card.find_by(id: params[:id])
+        render json: @card
     end
     
     def create
@@ -16,10 +18,19 @@ class Api::CardsController < ApplicationController
             render json: { message: card.errors }, status: 400
         end
     end
+    
+    def edit
+        @card.update(card_params)
+    end
+
+    def delete
+        @card.destroy
+    end
 
     private
     def set_card
         #find the card by the id here
+        @card = Card.find_by(id: params[:id])
     end
 
     def card_params
